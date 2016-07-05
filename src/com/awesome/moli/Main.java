98c666c7ad998ef19da516d0adbd53e9a9426c87
zhuwenxi <zhuwenxi8935@gmail.com>
2016-07-05 22:08:16 +0800

    Add some unit tests.

--- src/com/awesome/moli/Main.java
+++ src/com/awesome/moli/Main.java
@@ -3,45 +3,64 @@ package com.awesome.moli;
 public class Main {
 	public static void main(String[] args) {
 		
-		Moli.describe("Testcase1", new TestSuites() {
-			
+		Moli.describe("Testcases 1", new TestSuites() {
 			public void run() {
 				it("testcase 1", new TestSuite() {
-
 					public void run() {
 						expect("aa").toBe("b");
 						expect("aa").toBe("a");
 					}
-					
-					
 				});
 				
 				it("testcase 2", new TestSuite() {
-
 					public void run() {
 						expect("aa").toBe("aa");
 					}
-					
 				});
 				
 				it("testcase 3", new TestSuite() {
-
 					public void run() {
 						expect(1).toBe(1);
 					}
-					
 				});
 				
 				it("testcase 4", new TestSuite() {
-
 					public void run() {
 						expect(null).toBe(null);
 					}
 					
 				});
-				
 			}
-			
+		});
+		
+		Moli.describe("Testcases 2", new TestSuites() {
+			public void run() {
+				it("testcase 1", new TestSuite() {
+					public void run() {
+						expect("aa").toBe("b");
+						expect("aa").toBe("a");
+					}
+				});
+				
+				it("testcase 2", new TestSuite() {
+					public void run() {
+						expect("aa").toBe("aa");
+					}
+				});
+				
+				it("testcase 3", new TestSuite() {
+					public void run() {
+						expect(1).toBe(1);
+					}
+				});
+				
+				it("testcase 4", new TestSuite() {
+					public void run() {
+						expect(null).toBe(null);
+					}
+					
+				});
+			}
 		});
 	}
 }
